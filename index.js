9cd276a2d471a9cad4b6c9429fd58c8840127d50
sh-byeon <sh.byeon@m2cloud.kr>
2019-02-28 21:02:08 +0900

    .. product for 진행중

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 function init () {
   // 서버요청
   var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", function() {
+       oReq.addEventListener("load", function() {
     var jsonObj = JSON.parse(this.responseText);
     var cate = jsonObj.items;
 
-
+    console.log(jsonObj);
     // 카테고리 생성
     var cateUl = document.querySelector(".categories ul");
     var cateLi = document.querySelector("#categori-list");
@@ -22,7 +22,6 @@ function init () {
       active(evt) ;
     })
   });
-  // oReq.open("GET", "http://localhost:8080/api/promotions");//parameter를 붙여서 보낼수있음.
   oReq.open("GET", "./src/data/categori.json");//parameter를 붙여서 보낼수있음.
   oReq.send();
 
@@ -30,19 +29,47 @@ function init () {
 }
 
 function active(evt) {
-  // evt.target;
-  if (evt.target.className !== "active" ) {
-    evt.target.className = "active"
-  } else {
+  // console.log(evt.target.innerHTML);
 
-    evt.target.className = ""
-  }
+
+    var url = './src/data/products.json';
+    sendAjax(url);
 }
 
+function makeTemplate(data) {
+  console.log("ddddddddd",data.products);
+  var a = document.querySelector("#product-list");
+  var b = document.querySelector(".products ul");
+    var datas = data.products;
+    console.log(datas);
+      var result = a.innerHTML.replace("{description}", datas[0].description)
+                                .replace("{placeName}",datas[0].placeName)
+                                .replace("{content}",datas[0].content);
 
+    console.log('aa', result);
 
+    b.innerHTML += result;
 
+  /*for(var i =0; i<=datas.length; i++){
 
+      result += a.innerHTML.replace("{description}", datas[i].description)
+                             .replace("{placeName}",datas[i].placeName)
+                             .replace("{content}",datas[i].content)
+  };*/
+  // b.innerHTML += result;
+}
+
+// 클릭시 promotion을 가져온다
+function sendAjax(url) {
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', function() {
+      var data = JSON.parse(this.responseText);
+      // console.log("######", data);
+      makeTemplate(data);
+  });
+  oReq.open("GET", url);
+  oReq.send();
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Dom Loaded");
